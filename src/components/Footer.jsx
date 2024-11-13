8cd63709759d7cc4931990dd61477b67633c04cf
sirflint07 <fajobimichael123@gmail.com>
2024-11-13 16:27:35 +0100

    init commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <div className="footer-copyright">
+        <p>Copyright 2022 FlintSwap. All Rights Reserved</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
